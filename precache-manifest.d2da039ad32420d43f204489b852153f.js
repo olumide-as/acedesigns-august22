5b7943797c9d680dca1fda09cb23ee3222b9d9f1
olumide-as <adigunsamuel84@gmail.com>
2022-08-04 01:14:32 +0100

    Initial commit

---
+++ precache-manifest.d2da039ad32420d43f204489b852153f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f33a8bb5148cfdd29aa",
+    "url": "/static/css/main.8d546400.chunk.css"
+  },
+  {
+    "revision": "9f33a8bb5148cfdd29aa",
+    "url": "/static/js/main.a6caf0a2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a52add6fac005a8792d1",
+    "url": "/static/js/2.197a7b07.chunk.js"
+  },
+  {
+    "revision": "da6e59cf08c1512a87b7febb5f78e55e",
+    "url": "/static/media/Masking.da6e59cf.png"
+  },
+  {
+    "revision": "cf0918a36f01aab9696c9d5e683a2288",
+    "url": "/static/media/ace-light.cf0918a3.svg"
+  },
+  {
+    "revision": "aac86fe8010025d9e001aa64107a908a",
+    "url": "/index.html"
+  }
+];
